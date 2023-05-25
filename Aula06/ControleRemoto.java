5287702221fbf369524c6d3d44c62d9735fa66d6
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2023-05-25 18:13:01 -0300

    tentando java poo

---
+++ Aula06/ControleRemoto.java
@@ -0,0 +1,11 @@
+package Aula06;
+
+public class ControleRemoto {
+
+    public void ligar() {
+    }
+
+    public void abrirMenu() {
+    }
+
+}
---
+++ Aula06/Encapsulamento.java
@@ -0,0 +1,7 @@
+package Aula06;
+
+public class Encapsulamento {
+    public static void main(String[] args) {
+        
+    }
+}
