1c3b77f92037b1a93de58bb9254ac77fc2a58b23
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2023-06-05 17:00:05 -0300

    programação orientada a objetos

---
+++ Aula9/Publicacao.java
@@ -0,0 +1,11 @@
+package Aula9;
+
+public class Publicacao {
+    public void abrir();
+    public void fechar();
+    public void folhear(int p);
+    public void avancarPag();
+    public void voltarPag();
+    public interface java {
+    }
+}
